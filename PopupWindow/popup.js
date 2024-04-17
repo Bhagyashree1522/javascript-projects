e70c931dd285e1ca98835c24a6a0f29947923d0f
Bhagyashree Mangale <162953580+Bhagyashree1522@users.noreply.github.com>
2024-04-17 17:23:02 +0530

    popup window project

---
+++ PopupWindow/popup.js
@@ -0,0 +1,8 @@
+function openPopup(){
+    var popup=document.getElementById("popup");
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
